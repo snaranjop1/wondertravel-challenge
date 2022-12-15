ee86b1e63e5d55b272307d00546b4d9309fe8e78
Sergio Naranjo Puentes <sergionaranjopuentes@gmail.com>
2022-12-14 23:05:13 -0500

    Footer done

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css";
 import React from "react";
 import Banner from "./components/Banner/Banner";
 import MainContent from "./components/MainContent/MainContent";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <React.Fragment>
       <Banner />
       <MainContent />
+      <Footer />
     </React.Fragment>
   );
 }
